0536679b290195c72c056541bf05da5729f89efc
francescoTheSantis <dexo115bis@gmail.com>
2025-06-30 08:25:42 +0200

    eliminate key

--- env.py
+++ env.py
@@ -20,7 +20,7 @@ DATA_PATH = "/home/fdesantis/datasets" # Path to the datasets
 env['HYDRA_FULL_ERROR'] = '1'
 
 # Your OpenAI API key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 
 # Your Mistral AI API key
 MISTRALAIKEY = ""
